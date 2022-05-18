7f60650bcd5789285f9e44e7b77a199b620c9971
BuczkoBalazs <buci85@gmail.com>
2022-05-18 13:59:30 +0200

    Final push.

--- frontend/script.js
+++ frontend/script.js
@@ -75,16 +75,18 @@ const header = (logo) => {
 
 const card = (index, sub, title, text, buttonText) => {
     return`
-        <div class="card-container">
-            <p class="number-container">${index}</p>
-            <p class="sub">${sub}</p>
-            <h2 class="title">${title}</h2>
-            <article class="text">${text}</article>
-        </div>
-        <div class="button-container">
-            <button>${buttonText}</button>
-            <span class="material-symbols-outlined">arrow_forward</span>
-        </div>
+		<div class="card-container">
+			<div class="text-container">
+				<p class="number-container">${index+1}</p>
+				<p class="sub">${sub}</p>
+				<h2 class="title">${title}</h2>
+				<article class="text">${text}</article>
+			</div>
+			<div class="button-container">
+				<button>${buttonText}</button>
+				<span class="material-symbols-outlined">arrow_forward</span>
+			</div>
+		</div>
     `
 }
 
